632a494c96068f4acee47972978e3cdd1b5d3ec4
david <dafuentesc@unal.edu.co>
2024-09-10 11:57:59 -0500

    Primera versión de la carpeta

---
+++ Principal.java
@@ -0,0 +1,11 @@
+
+package Figuras;
+
+public class Principal{
+    public static void main(String [] args){
+        
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
+        
+    }
+}
